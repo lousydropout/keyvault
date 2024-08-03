a35cb9828414942fdc0e57bc8a47a2fff93be086
lousydropout <lousydropout@gmail.com>
2024-08-02 19:06:26 -0700

    add link back to homepage

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,7 +3,9 @@ import { Connect } from "./connect";
 export const Header = () => {
   return (
     <div className="flex items-end justify-between mt-4">
-      <h1 className="text-violet-400 font-semibold text-5xl">keyvault</h1>
+      <a href="/" className="text-violet-400 font-semibold text-5xl">
+        keyvault
+      </a>
       <Connect />
     </div>
   );
