cd87ce58ae39b24e97dfbf010843eadfd450561f
lousydropout <lousydropout@gmail.com>
2024-09-15 18:36:48 -0700

    import source code for shamir secret sharing

---
+++ browser-extension/src/utils/shamir.test.ts
@@ -0,0 +1,166 @@
+// taken from https://github.com/paritytech/banana_split/blob/master/tests/unit/crypto.spec.ts
+import { share, parse, reconstruct } from "@/utils/shamir";
+
+test("encodes-decodes empty values", () => {
+  const shards = share({
+    data: "",
+    title: "",
+    passphrase: "",
+    totalShards: 3,
+    requiredShards: 2,
+  });
+  const parsed = shards.map((s) => parse(s));
+  const reconstructed = reconstruct(parsed, "");
+  expect(reconstructed).toBe("");
+});
+
+test("properly deserializes metadata", () => {
+  const shards = share({
+    data: "Secret message",
+    title: "Secret title",
+    passphrase: "correct horse battery staple",
+    totalShards: 5,
+    requiredShards: 3,
+  });
+  const parsed = shards.map((s) => parse(s));
+  parsed.forEach((message) => {
+    expect(message.title).toBe("Secret title");
+    expect(message.requiredShards).toBe(3);
+    expect(message.nonce).toBe(parsed[0].nonce);
+  });
+});
+
+test("works with any 2 shards out of 3", () => {
+  const shards = share({
+    data: "Secret message",
+    title: "Secret title",
+    passphrase: "correct horse battery staple",
+    totalShards: 3,
+    requiredShards: 2,
+  });
+  const parsed = shards.map((s) => parse(s));
+  [
+    [0, 1],
+    [0, 2],
+    [1, 2],
+    [1, 0],
+    [2, 0],
+    [2, 1],
+  ].forEach(([i, j]) => {
+    expect(
+      reconstruct([parsed[i], parsed[j]], "correct horse battery staple")
+    ).toBe("Secret message");
+  });
+});
+
+test("fails with incorrect password", () => {
+  const shards = share({
+    data: "Secret message",
+    title: "Secret title",
+    passphrase: "correct horse battery staple",
+    totalShards: 5,
+    requiredShards: 3,
+  });
+  const parsed = shards.map((s) => parse(s));
+  expect(() => reconstruct(parsed, "")).toThrow("Unable to decrypt the secret");
+});
+
+test("works with unicode strings", () => {
+  const shards = share({
+    data: "Текст сообщения",
+    title: "Это секрет",
+    passphrase: "correct horse battery staple",
+    totalShards: 5,
+    requiredShards: 3,
+  });
+  const parsed = shards.map((s) => parse(s));
+  parsed.forEach((message) => {
+    expect(message.title).toBe("Это секрет");
+  });
+
+  const reconstructed = reconstruct(parsed, "correct horse battery staple");
+  expect(reconstructed).toBe("Текст сообщения");
+});
+
+test("reconstructs the reference example of v0 serialization", () => {
+  const shards = [
+    '{"t":"Very secret info","r":2,"d":"803c12929ba469d720a63fc8ca6f6ef1cc441f8f0b830ea04f8a484169ec800e4a7","n":"29abbb2c509adedb470f6d3fd3d083362b8c1a9283adf987"}',
+    '{"t":"Very secret info","r":2,"d":"801b4196813bb664141169f939414fec30c7f343326d8df45e557b25b0249fd43e2","n":"29abbb2c509adedb470f6d3fd3d083362b8c1a9283adf987"}',
+    '{"t":"Very secret info","r":2,"d":"80275318760b66ee5a1d7430dbf8769fda05e9e1ff7447eaa78539fbed006f0390b","n":"29abbb2c509adedb470f6d3fd3d083362b8c1a9283adf987"}',
+  ].map((s) => parse(s));
+
+  [
+    [0, 1],
+    [0, 2],
+    [1, 2],
+    [1, 0],
+    [2, 0],
+    [2, 1],
+  ].forEach(([i, j]) => {
+    expect(
+      reconstruct([shards[i], shards[j]], "amazing-daring-panda-horror")
+    ).toBe("Any questions?");
+  });
+});
+
+test("reconstructs the reference example of v1 serialization", () => {
+  const shards = [
+    '{"v":1,"t":"Pssst","r":2,"d":"8AdI3F1Xn4CK9mXEVWLWgg2gzho5WV38E/hn1OYyRMZenL/Jm6dmrZoiji2ZlMSVEW+XN9WW1I/ilDC1yiu4oBa4=","n":"a17TDZHP2iL/sdPHgFJUP3NlAC7bDgrp"}',
+    '{"v":1,"t":"Pssst","r":2,"d":"8ArluLqrT3URnL+IqsHddG9MpXqvSNt5JBLfTqSJmCg4raIFLg2XhfbnFLCTgCumI4qByThq9bBxnwLy8EgEHYiw=","n":"a17TDZHP2iL/sdPHgFJUP3NlAC7bDgrp"}',
+    '{"v":1,"t":"Pssst","r":2,"d":"8A2tZOf80PWbatpM/6ML9mLrUFkOu4kpyUiY62bPA6HmkVVtQpfosdF3nuhpo6K3MfmjsJ8ROokDShDgNka/ptFI=","n":"a17TDZHP2iL/sdPHgFJUP3NlAC7bDgrp"}',
+  ].map((s) => parse(s));
+
+  [
+    [0, 1],
+    [0, 2],
+    [1, 2],
+    [1, 0],
+    [2, 0],
+    [2, 1],
+  ].forEach(([i, j]) => {
+    expect(reconstruct([shards[i], shards[j]], "excess-torch-unfold-fix")).toBe(
+      "Version one is all-around better"
+    );
+  });
+});
+
+test("throws an error if nonces mismatch", () => {
+  const shards = share({
+    data: "Message",
+    title: "Title",
+    passphrase: "correct horse battery staple",
+    totalShards: 3,
+    requiredShards: 2,
+  }).map((s) => parse(s));
+  shards[0].nonce = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+  expect(() =>
+    reconstruct([shards[0], shards[1]], "correct horse battery staple")
+  ).toThrow(/Nonces mismatch/);
+});
+
+test("throws an error if titles mismatch", () => {
+  const shards = share({
+    data: "Message",
+    title: "Title",
+    passphrase: "correct horse battery staple",
+    totalShards: 3,
+    requiredShards: 2,
+  }).map((s) => parse(s));
+  shards[0].title = "Completely different title";
+  expect(() =>
+    reconstruct([shards[0], shards[1]], "correct horse battery staple")
+  ).toThrow(/Titles mismatch/);
+});
+
+test("doesn't work if less shards than necessary are supplied", () => {
+  const shards = share({
+    data: "Message",
+    title: "Title",
+    passphrase: "correct horse battery staple",
+    totalShards: 5,
+    requiredShards: 3,
+  }).map((s) => parse(s));
+  expect(() =>
+    reconstruct([shards[0], shards[1]], "correct horse battery staple")
+  ).toThrow(/Not enough shards/);
+});
---
+++ browser-extension/src/utils/shamir.ts
@@ -0,0 +1,236 @@
+// Taken from https://github.com/paritytech/banana_split/blob/master/src/util/crypto.ts
+import base64 from "base64-js";
+import crypto from "tweetnacl";
+import scrypt from "scryptsy";
+import secrets from "secrets.js-grempe";
+
+export type Shard = {
+  data: string;
+  version: number;
+  title: string;
+  nonce: string;
+  requiredShards: number;
+};
+
+type EncryptedData = {
+  value: Uint8Array;
+  nonce: Uint8Array;
+  salt: Uint8Array;
+};
+
+const HexEncodeArray = [
+  "0",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+];
+
+function strToUint8Array(str: string): Uint8Array {
+  return new TextEncoder().encode(str);
+}
+
+function uint8ArrayToStr(arr: Uint8Array): string {
+  return new TextDecoder("utf-8").decode(arr);
+}
+
+function hashString(str: string): Uint8Array {
+  return crypto.hash(strToUint8Array(str));
+}
+
+function hexify(arr: Uint8Array): string {
+  let s = "";
+  for (let i = 0; i < arr.length; i++) {
+    // `i` is a numerical counter for the loop and is never changed outside of there
+    // therefore `i` is numerical, and is safe to use as an array index
+    // eslint-disable-next-line security/detect-object-injection
+    const code = arr[i];
+    s += HexEncodeArray[code >>> 4];
+    s += HexEncodeArray[code & 0x0f];
+  }
+  return s;
+}
+
+function dehexify(str: string): Uint8Array {
+  const arr = new Uint8Array(str.length / 2);
+  for (let i = 0; i < arr.length; i++) {
+    // `i` is a numerical counter for the loop and is never changed outside of there
+    // therefore `i` is numerical, and is safe to use as an array index
+    // eslint-disable-next-line security/detect-object-injection
+    arr[i] = parseInt(str.slice(2 * i, 2 * i + 2), 16);
+  }
+  return arr;
+}
+
+function encrypt(
+  data: string,
+  salt: Uint8Array,
+  passphrase: string
+): EncryptedData {
+  const key = scrypt(passphrase, Buffer.from(salt), 1 << 15, 8, 1, 32);
+  const nonce = crypto.randomBytes(24);
+  return {
+    nonce,
+    salt,
+    value: crypto.secretbox(strToUint8Array(data), nonce, key),
+  };
+}
+
+function decrypt(
+  data: Uint8Array,
+  salt: Uint8Array,
+  passphrase: string,
+  nonce: Uint8Array
+): Uint8Array | null {
+  const key = scrypt(passphrase, Buffer.from(salt.buffer), 1 << 15, 8, 1, 32);
+  // This is a false positive, `secretbox.open` is unrelated to `fs.open`
+  // eslint-disable-next-line security/detect-non-literal-fs-filename
+  return crypto.secretbox.open(data, nonce, key);
+}
+
+/**
+ * Shares the given data using Shamir Secret Sharing algorithm.
+ *
+ * @param data - The data to be shared.
+ * @param title - The title of the shared data.
+ * @param passphrase - The passphrase used for encryption.
+ * @param totalShards - The total number of shards to be generated.
+ * @param requiredShards - The minimum number of shards required to reconstruct the data.
+ * @returns An array of strings representing the shared shards.
+ */
+function share({
+  data,
+  title,
+  passphrase,
+  totalShards,
+  requiredShards,
+}: {
+  data: string;
+  title: string;
+  passphrase: string;
+  totalShards: number;
+  requiredShards: number;
+}): string[] {
+  const salt = hashString(title),
+    encrypted = encrypt(data, salt, passphrase),
+    nonce = base64.fromByteArray(encrypted.nonce),
+    hexEncrypted = hexify(encrypted.value);
+  return secrets
+    .share(hexEncrypted, totalShards, requiredShards)
+    .map(function (shard: string) {
+      // First char is non-hex (base36) and signifies the bitfield size of our share
+      const encodedShard =
+        shard[0] + base64.fromByteArray(dehexify(shard.slice(1)));
+
+      return JSON.stringify({
+        v: 1,
+        t: title,
+        r: requiredShards,
+        d: encodedShard,
+        n: nonce,
+      }).replace(/[\u007F-\uFFFF]/g, function (chr) {
+        return "\\u" + ("0000" + chr.charCodeAt(0).toString(16)).substr(-4);
+      });
+    });
+}
+
+/**
+ * Parses a payload string and returns a Shard object.
+ *
+ * @param payload - The payload string to parse.
+ * @returns The parsed Shard object.
+ */
+function parse(payload: string): Shard {
+  let parsed = JSON.parse(payload);
+  return {
+    version: parsed.v || 0, // 'undefined' version is treated as 0
+    title: parsed.t,
+    requiredShards: parsed.r,
+    data: parsed.d,
+    nonce: parsed.n,
+  };
+}
+
+/**
+ * Reconstructs a secret using Shamir Secret Sharing algorithm.
+ *
+ * @param shardObjects - An array of shard objects containing the necessary information for reconstruction.
+ * @param passphrase - The passphrase used for decryption.
+ * @returns The reconstructed secret as a string.
+ * @throws {string} If there is a mismatch in the minimum shard requirements among shards.
+ * @throws {string} If there are not enough shards provided.
+ * @throws {string} If there is a mismatch in the nonces among shards.
+ * @throws {string} If there is a mismatch in the titles among shards.
+ * @throws {string} If there is a mismatch in the versions among shards.
+ * @throws {string} If the version is not supported.
+ * @throws {string} If unable to decrypt the secret.
+ */
+function reconstruct(shardObjects: Shard[], passphrase: string): string {
+  const shardsRequirements = shardObjects.map((shard) => shard.requiredShards);
+  if (!shardsRequirements.every((r) => r === shardsRequirements[0])) {
+    throw "Mismatching min shards requirement among shards!";
+  }
+  if (shardObjects.length < shardsRequirements[0]) {
+    throw `Not enough shards, need ${shardsRequirements[0]} but only ${shardObjects.length} provided`;
+  }
+
+  const nonces = shardObjects.map((shard) => shard.nonce);
+  if (!nonces.every((n) => n === nonces[0])) {
+    throw "Nonces mismatch among shards!";
+  }
+
+  const titles = shardObjects.map((shard) => shard.title);
+  if (!titles.every((t) => t === titles[0])) {
+    throw "Titles mismatch among shards!";
+  }
+
+  const versions = shardObjects.map((shard) => shard.version);
+  if (!versions.every((v) => v === versions[0])) {
+    throw "Versions mismatch along shards!";
+  }
+
+  let decryptedMsg: Uint8Array | null;
+
+  switch (versions[0]) {
+    case 0: {
+      const shardData = shardObjects.map((shard) => shard.data);
+      const encryptedSecret = secrets.combine(shardData);
+      const secret = dehexify(encryptedSecret);
+      const nonce = dehexify(nonces[0]);
+      const salt = hashString(titles[0]);
+      decryptedMsg = decrypt(secret, salt, passphrase, nonce);
+      break;
+    }
+    case 1: {
+      const shardDataV1 = shardObjects.map(
+        (shard) =>
+          shard.data[0] + hexify(base64.toByteArray(shard.data.slice(1)))
+      );
+      const encryptedSecretV1 = secrets.combine(shardDataV1);
+      const secretV1 = dehexify(encryptedSecretV1);
+      const nonceV1 = base64.toByteArray(nonces[0]);
+      const saltV1 = hashString(titles[0]);
+      decryptedMsg = decrypt(secretV1, saltV1, passphrase, nonceV1);
+      break;
+    }
+    default:
+      throw "Version is not supported!";
+  }
+  if (!decryptedMsg) {
+    throw "Unable to decrypt the secret";
+  }
+  return uint8ArrayToStr(decryptedMsg);
+}
+
+export { share, parse, reconstruct };
