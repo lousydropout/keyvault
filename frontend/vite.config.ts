746538d0e2020364929d497cd001abd61a360b96
lousydropout <lousydropout@gmail.com>
2024-10-07 18:19:33 -0700

    update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,18 @@
-import path from "path";
+import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill";
 import react from "@vitejs/plugin-react";
+import path from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      ...NodeGlobalsPolyfillPlugin({
+        process: true,
+        buffer: true,
+      }),
+    },
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
